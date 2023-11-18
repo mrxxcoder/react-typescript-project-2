40d1bccaebaaf23aa670d011683d1f821c368fd7
lowkeylu <ahmedsamy446x@gmail.com>
2023-11-19 01:40:48 +0200

    Toggle error message

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import ProductCard from "./components/ProductCard";
 import Modal from "./components/ui/Modal";
 import { formInputsList, productList } from "./data";
 import Button from "./components/ui/Button";
 import Input from "./components/ui/Input";
+import { IProduct } from "./interfaces";
+import { productValidation } from "./validation";
+import ErrorMessage from "./components/ui/ErrorMessage";
 
 const App = () => {
+  const initialProduct = {
+    title: "",
+    description: "",
+    imageURL: "",
+    price: "",
+    colors: [],
+    category: {
+      name: "",
+      imageURL: "",
+    },
+  };
+  // eslint-disable-next-line
+  const [product, setProduct] = useState<IProduct>(initialProduct);
+  const [errors, setErrors] = useState({
+    title: "",
+    description: "",
+    imageURL: "",
+    price: "",
+  });
   const [isOpen, setIsOpen] = useState(false);
 
   function closeModal() {
@@ -16,6 +38,41 @@ const App = () => {
     setIsOpen(true);
   }
 
+  function onChangeHandler(event: ChangeEvent<HTMLInputElement>) {
+    const { value, name } = event.target;
+
+    setProduct({
+      ...product,
+      [name]: value,
+    });
+
+    setErrors({
+      ...product,
+      [name]: "",
+    });
+  }
+
+  function submitHandler(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    const errors = productValidation(product);
+
+    const hasErrorMsg =
+      Object.values(errors).some((value) => value === "") &&
+      Object.values(errors).every((value) => value === "");
+
+    if (!hasErrorMsg) {
+      setErrors(errors);
+      return;
+    }
+
+    console.log("SEND DATA TO SERVER");
+  }
+
+  function onCancel() {
+    setProduct(initialProduct);
+    closeModal();
+  }
+
   const renderFormInputList = formInputsList.map((input) => (
     <div key={input.id} className="flex flex-col">
       <label
@@ -24,7 +81,14 @@ const App = () => {
       >
         {input.label}
       </label>
-      <Input type="text" id={input.id} name={input.name} />
+      <Input
+        type="text"
+        id={input.id}
+        name={input.name}
+        value={product[input.name]}
+        onChange={onChangeHandler}
+      />
+      <ErrorMessage msg={errors[input.name]} />
     </div>
   ));
 
@@ -41,11 +105,13 @@ const App = () => {
         {renderProductsList}
       </div>
       <Modal isOpen={isOpen} closeModal={closeModal} title="ADD A NEW PRODUCT">
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={submitHandler}>
           {renderFormInputList}
           <div className="flex items-center space-x-3">
             <Button className="bg-indigo-700">Submit</Button>
-            <Button className="bg-gray-500">Cancel</Button>
+            <Button className="bg-gray-500" onClick={onCancel}>
+              Cancel
+            </Button>
           </div>
         </form>
       </Modal>
